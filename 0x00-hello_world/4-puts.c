fcae0f427b23edae8d7a0d8139fe533a8a0b07c0
wieziana <talk2mewisdom@yahoo.ie>
2023-02-21 17:34:17 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  use of put function in place of printf
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
