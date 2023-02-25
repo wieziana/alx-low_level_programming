0974f775131dc01bbcfa2479099b80e4096f976c
wieziana <talk2mewisdom@yahoo.ie>
2023-02-24 16:19:27 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Prints alpabeths in lowercass except
+ * of the q and e characters using the
+ * putchar functions only.
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			continue;
+			n++;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
