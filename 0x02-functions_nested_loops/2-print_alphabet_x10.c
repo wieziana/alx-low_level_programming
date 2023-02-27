bb6fe26f8e1246df0270262f8c72ff557ae879b8
wieziana <talk2mewisdom@yahoo.ie>
2023-02-27 14:17:26 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print all alphabeth 10 times
+ * but in lower case
+ */
+
+void print_alphabet_x10(void)
+{
+	int n;
+	char m;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (m = 'a'; m <= 'z'; m++)
+		{
+			_putchar(m);
+		}
+		_putchar('\n')
+	}
+}
