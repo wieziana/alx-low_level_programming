a9ed0bdcb5c1f363e1f1444afd997402dad7205b
wieziana <talk2mewisdom@yahoo.ie>
2023-02-28 21:33:14 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -19,14 +19,9 @@ void rev_string(char *s)
 	for (i = 0; i < count; i++)
 	{
 		s--;
-
-		for (i = 0; i < (count / 2); i++)
-		{
-			int last = count - 1;
-
-			last_char = s[last - i];
-			s[last - i] = s[i];
-			s[i] = last_char;
-		}
+		int last = count - 1;
+		last_char = s[last - i];
+		s[last - i] = s[i];
+		s[i] = last_char;
 	}
 }
